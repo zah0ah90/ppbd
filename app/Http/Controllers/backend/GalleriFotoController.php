e839727820e8e749f59618cc811082b45abef674
zah0ah <zah0ah@gmail.com>
2021-12-17 17:46:13 +0700

    uppload gambar guru

--- app/Http/Controllers/backend/GalleriFotoController.php
+++ app/Http/Controllers/backend/GalleriFotoController.php
@@ -8,14 +8,17 @@ use Illuminate\Http\Request;
 
 class GalleriFotoController extends Controller
 {
-    /**
+  /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $gallerifoto = Gallerifoto::paginate(10);
+        // $user = User::where('person_id', '=', 1);
+        
+        return view('backend.galleri_foto.index', ['gallerifoto' => $gallerifoto]);
     }
 
     /**
@@ -25,7 +28,7 @@ class GalleriFotoController extends Controller
      */
     public function create()
     {
-        //
+        return view('backend.galleri_foto.tambah');
     }
 
     /**
@@ -35,17 +38,41 @@ class GalleriFotoController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
+    {        
+        $request->validate([
+            'nama' => 'required',
+            'jenis_kelamin' => 'required',
+            'tanggal_lahir' => 'required',
+            'wali_kelas' => 'required',
+            'mengajar_sejak' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        $input = $request->all();
+        if ($image = $request->file('image')) {
+            $destinationPath = 'backend/image/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }
+
+        $gallerifoto = Gallerifoto::create($input);
+        $gallerifoto->id;
+
+        if($gallerifoto->save()) {
+            return redirect()->route('gallerifoto.index')->with('success', 'Berhasil menambahkan data galleri foto');
+        } else {
+            return redirect()->back()->with('error', 'Gagal menambahkan data galleri foto');
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\GalleriFoto  $galleriFoto
+     * @param  \App\Models\Gallerifoto  $gallerifoto
      * @return \Illuminate\Http\Response
      */
-    public function show(GalleriFoto $galleriFoto)
+    public function show(Gallerifoto $gallerifoto)
     {
         //
     }
@@ -53,34 +80,64 @@ class GalleriFotoController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\GalleriFoto  $galleriFoto
+     * @param  \App\Models\Gallerifoto  $gallerifoto
      * @return \Illuminate\Http\Response
      */
-    public function edit(GalleriFoto $galleriFoto)
+    public function edit(Gallerifoto $gallerifoto)
     {
-        //
+        return view('backend.gallerifoto.edit',compact('gallerifoto'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\GalleriFoto  $galleriFoto
+     * @param  \App\Models\Gallerifoto  $gallerifoto
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, GalleriFoto $galleriFoto)
+    public function update(Request $request, Gallerifoto $gallerifoto)
     {
-        //
+        $request->validate([
+            'nama' => 'required',
+            'jenis_kelamin' => 'required',
+            'tanggal_lahir' => 'required',
+            'wali_kelas' => 'required',
+            'mengajar_sejak' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        $input = $request->all();
+        if ($image = $request->file('image')) {
+
+            $destinationPath = 'backend/image/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }else{
+            unset($input['image']);
+        }
+
+        $gallerifoto->update($input);
+
+        if($gallerifoto) {
+            return redirect()->route('gallerifoto.index')->with('success', 'Berhasil memperbarui data gallerifoto');
+        } else {
+            return redirect()->back()->with('error', 'Gagal memperbarui data gallerifoto');
+        }
+
+        // $product->update($request->all());
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\GalleriFoto  $galleriFoto
+     * @param  \App\Models\Gallerifoto  $gallerifoto
      * @return \Illuminate\Http\Response
      */
-    public function destroy(GalleriFoto $galleriFoto)
+    public function destroy(Gallerifoto $gallerifoto)
     {
-        //
+        $gallerifoto->delete();
+        return redirect()->back()->with('success', 'Berhasil Menghapus!');
+        // return redirect()->route('products.index')->with('success','Product deleted successfully');
     }
 }
