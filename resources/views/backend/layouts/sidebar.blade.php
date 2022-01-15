3edccf4530e24368d06e3017e43fe21db070aed4
zah0ah <zah0ah@gmail.com>
2022-01-15 16:09:36 +0700

    panambahan

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -63,7 +63,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ route('pengumuman.index') }}" class="nav-link ">
+                            <a href="{{ route('berita.index') }}" class="nav-link ">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Berita</p>
                             </a>
