f382ebdf36222ccf0c7a9b989f3c096ed1601e82
zah0ah <zah0ah@gmail.com>
2022-01-15 16:26:24 +0700

    gmaps

--- resources/views/frontend/page-belum-login/kontak.blade.php
+++ resources/views/frontend/page-belum-login/kontak.blade.php
@@ -1,69 +1,70 @@
 @extends('frontend.page-belum-login.layouts.app')
 @section('content')
-<div style="font-size: 26px;"><b>KONTAK</b></div>
-<div class="dropdown-divider" style="border: 3px solid rgba(0, 0, 0, 0.2);"></div>
-<br>
-<div class="row">
+    <div style="font-size: 26px;"><b>KONTAK</b></div>
+    <div class="dropdown-divider" style="border: 3px solid rgba(0, 0, 0, 0.2);"></div>
+    <br>
+    <div class="row">
 
-    <div class="col-6">
-        <div class="row">
-            <div class="col-sm-2">
-                <i class="fas fa-landmark" style="margin-top: 8px;"></i>
+        <div class="col-6">
+            <div class="row">
+                <div class="col-sm-2">
+                    <i class="fas fa-landmark" style="margin-top: 8px;"></i>
+                </div>
+                <div class="col-sm-10">
+                    <b>Alamat</b>
+                    <p>Kp. Kawung Luwuk RT.1 RW.1
+                        Kel. Sukamanah, Kec. Rongga </p>
+                </div>
             </div>
-            <div class="col-sm-10">
-                <b>Alamat</b>
-                <p>Kp. Kawung Luwuk RT.1 RW.1
-                    Kel. Sukamanah, Kec. Rongga </p>
+            <div class="row">
+                <div class="col-sm-2">
+                    <i class="fas fa-phone-alt" style="margin-top: 8px;"></i>
+                </div>
+                <div class="col-sm-10">
+                    <b>Telepon</b>
+                    <p>081255554444</p>
+                </div>
             </div>
+            <div class="row">
+                <div class="col-sm-2">
+                    <i class="far fa-envelope" style="margin-top: 8px;"></i>
+                </div>
+                <div class="col-sm-10">
+                    <b>Email</b>
+                    <p>sdnnurulsalam@gmail.com</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2">
+                    <i class="fas fa-globe-asia" style="margin-top: 8px;"></i>
+                </div>
+                <div class="col-sm-10">
+                    <b>Media Sosial</b>
+                    <p>www.sdnnurulsalam.ac.id</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2">
+                    <i class="far fa-newspaper" style="margin-top: 8px;"></i>
+                </div>
+                <div class="col-sm-10">
+                    <b>Media Sosial</b>
+                    <p>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-facebook"></i>
+                    </p>
+                </div>
+            </div>
+
         </div>
-        <div class="row">
-            <div class="col-sm-2">
-                <i class="fas fa-phone-alt" style="margin-top: 8px;"></i>
-            </div>
-            <div class="col-sm-10">
-                <b>Telepon</b>
-                <p>081255554444</p>
-            </div>
+        <div class="col-6">
+
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15840.732069556056!2d107.2780523!3d-6.9877111!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf969a77648b6327c!2sSDN%20Nurul%20Salam!5e0!3m2!1sid!2sid!4v1642238727850!5m2!1sid!2sid"
+                width="450" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
-        <div class="row">
-            <div class="col-sm-2">
-                <i class="far fa-envelope" style="margin-top: 8px;"></i>
-            </div>
-            <div class="col-sm-10">
-                <b>Email</b>
-                <p>sdnnurulsalam@gmail.com</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-2">
-                <i class="fas fa-globe-asia" style="margin-top: 8px;"></i>
-            </div>
-            <div class="col-sm-10">
-                <b>Media Sosial</b>
-                <p>www.sdnnurulsalam.ac.id</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-2">
-                <i class="far fa-newspaper" style="margin-top: 8px;"></i>
-            </div>
-            <div class="col-sm-10">
-                <b>Media Sosial</b>
-                <p>
-                    <i class="fab fa-instagram"></i>
-                    <i class="fab fa-facebook"></i>
-                </p>
-            </div>
-        </div>
+
 
     </div>
-    <div class="col-6">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7932.927029354859!2d106.85452570616442!3d-6.202421490157217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f468f934f5fb%3A0x251e626ea3d5b24d!2sJayakarta%20University!5e0!3m2!1sid!2sid!4v1638368550032!5m2!1sid!2sid"
-            width="450" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-    </div>
-
-
-</div>
 
 @endsection
